33155943b1a27f049d6130218c09cf1ce3964f61
Markus Netz <markus.netz.89@gmail.com>
2024-03-21 21:16:18 +0100

    Add files via upload

--- number_guess.sh
+++ number_guess.sh
@@ -17,7 +17,7 @@ number_of_guesses=0
 setup_game_db(){
   # CREATE DATABASE number_guess ;
   $PSQL "CREATE TABLE if not exists players (player_id SERIAL PRIMARY KEY, name VARCHAR(30) NOT NULL, REGISTERED DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP);"
-  $PSQL "CREATE TABLE if not exists games (game_id SERIAL PRIMARY KEY, player_id BIGINT UNSIGNED NOT NULL, winning_number SMALLINT UNSIGNED NOT NULL, total_guesses SMALLINT UNSIGNED NOT NULL, difficulty ENUM('easy','intermediate','hard') NOT NULL DEFAULT 'easy' );"
+  $PSQL "CREATE TABLE if not exists games (game_id SERIAL PRIMARY KEY, player_id bigint unsigned NOT NULL, winning_number smallint unsigned NOT NULL, total_guesses smallint unsigned NOT NULL, difficulty ENUM('easy','intermediate','hard') NOT NULL DEFAULT 'easy', finished_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP);"
 }
 
 select_language() {
@@ -33,34 +33,36 @@ select_language() {
   elif [[ "${input_lang}" -eq 2 ]]; then
     lang="sv"
   else
+    echo "Invalid choice - defaulting to English"
     lang="en"
   fi
 }
 
 select_difficulty() {
 
-  if [[ "${lang}" == "en" ]]; then
-    echo ":: Select difficulty ::"
-    echo " 1) Easy         (1-1000)"
-    echo " 2) Intermediate (1-3333)"
-    echo " 3) Hard         (1-127, with no help)"
-  elif [[ "${lang}" == "sv" ]]; then
-    echo ":: Välj svårighetsgrad ::"
-    echo " 1) Lätt         (1-1000)"
-    echo " 2) Mellan       (1-3333)"
-    echo " 3) Svår         (1-127, ingen hjälp)"
-  fi
+  echo ":: $prompt_diff_header ::"
+  echo "$prompt_diff_1"
+  echo "$prompt_diff_2"
+  echo "$prompt_diff_3"
 
   read input_difficulty
 
   if [[ "${input_difficulty}" -eq 1 ]]; then
     difficulty="easy"
+    readonly min_secret_value=1
+    readonly max_secret_value=1000
   elif [[ "${input_difficulty}" -eq 2 ]]; then
     difficulty="intermediate"
+    readonly min_secret_value=1
+    readonly max_secret_value=3333
   elif [[ "${input_difficulty}" -eq 3 ]]; then
     difficulty="hard"
+    readonly min_secret_value=1
+    readonly max_secret_value=127
   else
-    difficulty="easy"
+    echo "Invalid choice - defaulting to easy"
+    readonly min_secret_value=1
+    readonly max_secret_value=1000
   fi
 }
 
@@ -96,7 +98,7 @@ game_on() {
   prompt_user_guess
   
   if [[ "${USER_GUESS}" -eq "${secret_number}" ]]; then
-    ins=$( $PSQL "INSERT INTO games (player_id, winning_number, total_guesses) VALUES ((SELECT player_id FROM players WHERE name = '${USERNAME}'), '${secret_number}', '${number_of_guesses}' ) ;")
+    ins=$( $PSQL "INSERT INTO games (player_id, winning_number, total_guesses, difficulty) VALUES ((SELECT player_id FROM players WHERE name = '${USERNAME}'), '${secret_number}', '${number_of_guesses}', '${difficulty}' ) ;")
   elif [[ "${USER_GUESS}" -gt "${secret_number}" ]]; then
     print_guess_help $lower_guess
     game_on
@@ -130,14 +132,19 @@ set_prompts() {
   fi
 
   if [[ "${selected_lang}" == "en" ]]; then
-    prompt_username="Enter your username:"
+    prompt_diff_header="Select difficulty"
+    prompt_diff_1=" 1) Easy         (1-1000)"
+    prompt_diff_2=" 2) Intermediate (1-3333)"
+    prompt_diff_3=" 3) Hard         (1-127, with no help)"
+    prompt_username="Enter your username"
     prompt_new_user_1="Welcome, "
     prompt_new_user_2="! It looks like this is your first time here."
     prompt_returning_user_1="Welcome back, "
     prompt_returning_user_2="! You have played "
     prompt_returning_user_3=" games, and your best game took "
     prompt_returning_user_4=" guesses."
-    prompt_guess_start="Guess the secret number between ${min_secret_value} and ${max_secret_value}"
+    prompt_guess_start_1="Guess the secret number between ${min_secret_val} and ${max_secret_value}"
+    prompt_guess_start_2="and"
     nan_guess="That is not a number, guess again:"
     lower_guess="It's lower than that, guess again:"
     higher_guess="It's higher than that, guess again:"
@@ -151,14 +158,19 @@ set_prompts() {
     prompt_hard_close="Close..., guess again:"
     prompt_hard_closer="Very Close..., guess again:"
   elif [[ "${selected_lang}" == "sv" ]]; then
-    prompt_username="Ange ditt spelarnamn:"
+    prompt_diff_header="Välj svårighetsgrad"
+    prompt_diff_1=" 1) Lätt         (1-1000)"
+    prompt_diff_2=" 2) Mellan       (1-3333)"
+    prompt_diff_3=" 3) Svår         (1-127, ingen hjälp)"
+    prompt_username="Ange spelarnamn"
     prompt_new_user_1="Välkommen, "
     prompt_new_user_2="! Det verkar som att det är första gången."
     prompt_returning_user_1="Välkommen tillbaka, "
     prompt_returning_user_2="! Du har spelat "
     prompt_returning_user_3=" gånger förut och din bästa runda gjorde du på "
     prompt_returning_user_4=" försök."
-    prompt_guess_start="Gissa det hemliga numret mellan ${min_secret_value} och ${max_secret_value}:"
+    prompt_guess_start_1="Gissa det hemliga numret mellan"
+    prompt_guess_start_2="och"
     nan_guess="Du måste gissa på siffror, gissa igen:"
     lower_guess="Det är lägre, gissa igen:"
     higher_guess="Det är högre, gissa igen:"
@@ -182,22 +194,12 @@ setup_game_db
 
 echo
 select_language
+# set up variable values prompts to be printed, can set language.
+set_prompts "${lang}"
 
 echo
 # prompt player for difficulty leve
 select_difficulty
-
-if [[ "${difficulty}" == "intermediate" ]]; then
-  readonly min_secret_value=1
-  readonly max_secret_value=3333
-elif [[ "${difficulty}" == "hard" ]]; then
-  readonly min_secret_value=1
-  readonly max_secret_value=127
-else
-  readonly min_secret_value=1
-  readonly max_secret_value=1000
- difficulty="easy"
-fi
 
 # Will set the random number to guess for.
 secret_number=$((min_secret_value + RANDOM % max_secret_value))
@@ -206,11 +208,9 @@ calculated_higher=$((secret_number + 2))
 calculated_low=$((secret_number - 5))
 calculated_high=$((secret_number + 5))
 
-# set up variable values prompts to be printed, can set language.
-set_prompts "${lang}"
 
 echo
-echo "$prompt_username"
+echo ":: $prompt_username ::"
 read USERNAME
 # escape single qoutes.
 USERNAME=$( echo "$USERNAME" | sed -e "s/'/\\\'/g" )
@@ -230,11 +230,11 @@ else
   
   best_play_round=$( ${PSQL} "SELECT IFNULL(MIN(total_guesses),0) FROM games WHERE player_id = (SELECT player_id FROM players WHERE name = '${check_user}'); ")
   echo "${prompt_returning_user_1}${check_user}${prompt_returning_user_2}${tot_played}${prompt_returning_user_3}${best_play_round}${prompt_returning_user_4}"
-
+  USERNAME="${check_user}"
 fi
 
 echo
-echo "${prompt_guess_start}"
+echo "${prompt_guess_start_1} ${min_secret_value} ${prompt_guess_start_2} ${max_secret_value}:"
 
 # Start the game looping... :D
 game_on
---
+++ number_guess.sql
@@ -0,0 +1,31 @@
+--
+-- Database number_guess
+--
+CREATE DATABASE if not exists number_guess;
+
+USE number_guess;
+
+--
+-- Table structure for table `games`
+--
+
+DROP TABLE IF EXISTS `games`;
+CREATE TABLE `games` (
+  `game_id` SERIAL PRIMARY KEY,
+  `player_id` bigint unsigned NOT NULL,
+  `winning_number` smallint unsigned NOT NULL,
+  `total_guesses` smallint unsigned NOT NULL,
+  `difficulty` enum('easy','intermediate','hard') NOT NULL DEFAULT 'easy',
+  `finished_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+) ENGINE=InnoDB;
+
+--
+-- Table structure for table `players`
+--
+
+DROP TABLE IF EXISTS `players`;
+CREATE TABLE `players` (
+  `player_id` SERIAL PRIMARY KEY,
+  `name` varchar(30) NOT NULL,
+  `REGISTERED` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB;
